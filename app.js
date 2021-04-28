57f29be06ce38cc78294ff6bf8ab22d0d8fb5f7c
Alex Margorskij <margorskija@gmail.com>
2021-04-28 13:26:16 +0300

    rooms

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const authRouter = require('./routes/api/auth');
 const usersRouter = require('./routes/api/users');
 const mailChimpRouter = require('./routes/api/mailchimp');
 const votesRouter = require('./routes/api/votes');
+const roomsRouter = require('./routes/api/rooms');
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 const AVATARS_OF_USERS = process.env.AVATARS_OF_USERS;
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use('/auth', authRouter);
 app.use('/users', usersRouter);
 app.use('/votes', votesRouter);
+app.use('/rooms', roomsRouter);
 app.use('/mailchimp', mailChimpRouter);
 
 app.get('/', (req, res) => {
--- model/rooms.js
+++ model/rooms.js
@@ -1,46 +1,11 @@
-const User = require('./schemas/user.js');
+const Room = require('./schemas/room.js');
 
-const findByEmail = async email => {
-  return await User.findOne({ email: email });
-};
-const findByVerifyToken = async verifyToken => {
-  return await User.findOne({ verifyToken });
+const join = (roomId, userId) => {};
+const create = userId => {
+  return Room.create({ owner: userId });
 };
 
-const findById = async id => {
-  return await User.findOne({ _id: id });
-};
-const create = async ({ name, email, password, sex, verify, verifyToken }) => {
-  return await User.create({
-    name,
-    email,
-    password,
-    sex,
-    verify,
-    verifyToken,
-  });
-};
-
-const updateToken = async (id, token) => {
-  return await User.updateOne({ _id: id }, { token: token });
-};
-
-const updateVerifyToken = async (id, verify, verifyToken) => {
-  return await User.updateOne({ _id: id }, { verify, verifyToken });
-};
-
-const updateAvatar = async (id, avatar) => {
-  return await User.updateOne({ _id: id }, { avatar: avatar });
-};
-
-const verify = async () => {};
 module.exports = {
-  findByEmail,
-  findByVerifyToken,
+  join,
   create,
-  updateToken,
-  updateVerifyToken,
-  findById,
-  updateAvatar,
-  verify,
 };
